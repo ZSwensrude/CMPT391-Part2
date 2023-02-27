a3d8a5d59820cc3ff84c12d326d8c50d569c6401
ZSwensrude <97464094+ZSwensrude@users.noreply.github.com>
2023-02-27 16:14:46 -0700

    Initialize app with SQL, squashed bugs

--- 391warehouse/391warehouse.cs
+++ 391warehouse/391warehouse.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,52 @@ using System.Windows.Forms;
 
 namespace _391warehouse
 {
-    public partial class Form1 : Form
+    public partial class Warehouse : Form
     {
-        public Form1()
+
+        public SqlConnection myConnection;
+        public SqlCommand myCommand;
+        public SqlDataReader myReader;
+
+        public Warehouse()
         {
             InitializeComponent();
+            ///////////////////////////////
+            String connectionString = "Server = LAPTOP-HUT8634L; Database = 391warehouse; Trusted_Connection = yes;";
+            // Need to change server to your personal SQL server before using (and Database if different)
+            // Adam: DESKTOP-SO5MCT3
+            // Zach: LAPTOP-HUT8634L
+            // Jasper: LAPTOP-JPNKMR DESKTOP-JSPRNKM
+            // Salah: 
+            // Brittney: LAPTOP-L6HCRV5P
+
+            /* Starting the connection */
+            /*  SqlConnection myConnection = new SqlConnection("user id=temp2;" + // Username
+                                         "password=adminadmin;" + // Password
+                                         "server=localhost;" + // IP for the server
+                                                               //"Trusted_Connection=yes;" +
+                                         "database=ConnectTutorial; " + // Database to connect to
+                                         "connection timeout=30"); // Timeout in seconds */
+
+            SqlConnection myConnection = new SqlConnection(connectionString); // Timeout in seconds
+
+            try
+            {
+                myConnection.Open(); // Open connection
+                myCommand = new SqlCommand();
+                myCommand.Connection = myConnection; // Link the command stream to the connection
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "Error");
+                this.Close();
+            }
+
+        }
+
+        private void Warehouse_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
